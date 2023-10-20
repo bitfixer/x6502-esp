2684b83810da6740ec4c812f02af25b3785b5c69
Michael Hill <bitfixer@bitfixer.com>
2023-10-20 13:18:28 -0700

    use heap_caps_malloc

--- cpu.c
+++ cpu.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <esp_heap_caps.h>
+
 cpu * new_cpu(uint16_t pc_start) {
-    cpu *m = malloc(sizeof(cpu));
+    cpu* m = heap_caps_malloc(sizeof(cpu), MALLOC_CAP_SPIRAM);
     m->pc = pc_start;
     m->sr = FLAG_INTERRUPT;
     m->sp = 0xFF;
